be90c2c8fc6e95994b4f4ed7fe1ffcf8b57cb245
PokeManiacYug <52129410+PokeManiacYug@users.noreply.github.com>
2020-11-28 23:18:19 +0530

    Update rps.py
    added comments

--- rps.py
+++ rps.py
@@ -3,6 +3,8 @@ import random
 import sys
 import signal
 
+
+#user choses rock so this happens
 def rock(comp_choice):
     if comp_choice == 'PAPER':
         print('\nYOU LOSE')
@@ -11,6 +13,8 @@ def rock(comp_choice):
     else:
         print('\nYOU WIN')
 
+
+#user choses paper so this happens
 def paper(comp_choice):
     if comp_choice == 'PAPER':
         print('\nDRAW')
@@ -19,6 +23,8 @@ def paper(comp_choice):
     else:
         print('\nYOU LOSE')
 
+
+#user choses scissor so this happens
 def scissor(comp_choice):
     if comp_choice == 'PAPER':
         print('\nYOU WIN')
@@ -28,6 +34,8 @@ def scissor(comp_choice):
         print('\nDRAW')
 
 
+
+#taking input from user and asking for input agin if the input is worng
 def take_user_input():
     while True:
         user_choice = input('ROCK PAPER SCISSOR\n\n>>> ').lower()
@@ -39,34 +47,41 @@ def take_user_input():
             continue
     return user_choice
 
+
+
+#this is the brain
 def rockpaperscissor():
 
     cont = True
     while cont is True:
         
         user_choice = take_user_input()
-        comp = random.randint(1,4)
+        comp = random.randint(1,4)  #generating a random number from 1 to 3 and assigning each number to an entity
         if comp == 1:
             comp_choice = 'ROCK'
         elif comp == 2:
             comp_choice = 'PAPER'
         else:
             comp_choice = 'SCISSOR'
+
+
         print(f'\nComputer chose {comp_choice}')
-        if user_choice == 'rock':
+        
+        
+        if user_choice == 'rock':  #calling the appropriate function based on what the user chose        
             rock(comp_choice)
         elif user_choice == 'paper':
             paper(comp_choice)
         else:
             scissor(comp_choice)
 
-        ch = input('\nWant to play another round? (Y/N)\n').lower()
+        ch = input('\nWant to play another round? (Y/N)\n').lower()  #asking whether to play another round or not
         if ch == 'y':
             cont = True
             os.system('clear')
         else:
             cont = False
-            os.kill(os.getppid(), signal.SIGHUP)
+            os.kill(os.getppid(), signal.SIGHUP)  #closes the terminal
 
 
 if __name__ == "__main__":
